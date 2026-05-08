e30c77569df0b96dd8f3e75dc04d185348c8b643
Jesse Kanadi <jessekanadi@gmail.com>
2026-05-08 15:45:22 +0100

    order page

--- lib/components/widgets/orders.dart
+++ lib/components/widgets/orders.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:vibrant_commerce/components/assets/app_colors.dart';
 
 class Orders extends StatelessWidget {
-  const Orders({super.key});
+  final String status;
+  final String items;
+  final String price;
+  final Color color;
+  final Color statusColor;
+  final Color statusTextColor;
+  final String imagePath;
+  const Orders({
+    super.key,
+    required this.items,
+    required this.price,
+    required this.status,
+    required this.color,
+    required this.statusColor,
+    required this.statusTextColor,
+    required this.imagePath,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(16),
       width: double.infinity,
       height: 195,
       decoration: BoxDecoration(
@@ -18,6 +36,110 @@ class Orders extends StatelessWidget {
           ),
         ],
         borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Order ID',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+
+              Text(
+                'Order ID',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            ],
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('#ORD-12345', style: TextStyle(fontSize: 16)),
+
+              Text('Oct 24, 2023', style: TextStyle(fontSize: 16)),
+            ],
+          ),
+          Divider(thickness: 0.5),
+          const SizedBox(height: 16),
+          //info row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 78,
+                    height: 78,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: DecorationImage(
+                        image: AssetImage(imagePath),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 110,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(99),
+                          color: color,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 8,
+                              height: 8,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: statusColor,
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              status,
+                              style: TextStyle(
+                                color: statusTextColor,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(items, style: TextStyle(fontSize: 16)),
+                      Text(price, style: TextStyle(fontSize: 16)),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Details',
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14,
+                    color: AppColors.primaryColor,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/screens/main_screens/orders_page.dart
+++ lib/screens/main_screens/orders_page.dart
@@ -12,67 +12,97 @@ class OrdersPage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsetsGeometry.all(24),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //all orders
-                  Container(
-                    width: 110,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(99),
-                      color: AppColors.secondaryColor,
-                    ),
-                    child: Center(
-                      child: Text(
-                        'All Orders',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //all orders
+                    Container(
+                      width: 110,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(99),
+                        color: AppColors.secondaryColor,
                       ),
-                    ),
-                  ),
-                  //ongoing
-                  Container(
-                    width: 110,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(99),
-                      color: Color(0xffE4E4E8),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'On Going',
-                        style: TextStyle(
-                          color: Color(0xff444650),
-                          fontSize: 16,
+                      child: Center(
+                        child: Text(
+                          'All Orders',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
                         ),
                       ),
                     ),
-                  ),
-                  //completed
-                  Container(
-                    width: 110,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(99),
-                      color: Color(0xffE4E4E8),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'All Orders',
-                        style: TextStyle(
-                          color: Color(0xff444650),
-                          fontSize: 16,
+                    //ongoing
+                    Container(
+                      width: 110,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(99),
+                        color: Color(0xffE4E4E8),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'On Going',
+                          style: TextStyle(
+                            color: Color(0xff444650),
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Orders(),
-            ],
+                    //completed
+                    Container(
+                      width: 110,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(99),
+                        color: Color(0xffE4E4E8),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'All Orders',
+                          style: TextStyle(
+                            color: Color(0xff444650),
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Orders(
+                  imagePath: 'assets/images/watch.png',
+                  statusTextColor: Colors.green,
+                  statusColor: Colors.green,
+                  status: 'Delivered',
+                  color: Color(0xffDCFCE7),
+                  price: '\$173.5',
+                  items: '2 Items',
+                ),
+                const SizedBox(height: 16),
+                Orders(
+                  imagePath: 'assets/images/shoe.png',
+                  statusTextColor: Color(0xffC2410C),
+                  items: '4 itmes',
+                  price: '\$200',
+                  status: 'In transit',
+                  color: Color(0xffFFEDD5),
+                  statusColor: Color(0xffF97316),
+                ),
+                const SizedBox(height: 16),
+                Orders(
+                  imagePath: 'assets/images/headphone.png',
+                  statusTextColor: Color(0xffC2410C),
+                  items: '1 itmes',
+                  price: '\$197.50',
+                  status: 'In transit',
+                  color: Color(0xffFFEDD5),
+                  statusColor: Color(0xffF97316),
+                ),
+              ],
+            ),
           ),
         ),
       ),
