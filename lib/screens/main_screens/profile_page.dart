2fa15c5ce111aee0ebf3a1b0faf5bf7b78c3c97e
Jesse Kanadi <jessekanadi@gmail.com>
2026-05-19 15:38:23 +0100

    profile page

--- lib/screens/main_screens/profile_page.dart
+++ lib/screens/main_screens/profile_page.dart
@@ -8,14 +8,41 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.tertiaryColor,
-      appBar: AppBar(
-        title: const Text('My Profile'),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        foregroundColor: AppColors.neutralColor,
-      ),
-      body: const Center(
-        child: Text('Profile Page Placeholder'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Center(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/avatar.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      top: 70,
+                      child: Image.asset('assets/images/pic.png'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Alex Johnson',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
